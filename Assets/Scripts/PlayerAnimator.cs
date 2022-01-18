8e244319efe116a518512e4b3416c97271b27761
Martin Nguyen <45037438+martinphnguyen@users.noreply.github.com>
2022-01-18 22:23:22 +0000

    Update PlayerAnimator.cs
    Added some form of jumping animation but needs work to smoothen animation out

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -15,12 +15,19 @@ public class PlayerAnimator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        if(Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0) //checking if any movement key is pressed
         {
-            animator.SetBool("isRunning", true);
+            animator.SetBool("isRunning", true); //set our bool to true to allow animation
         }
         else 
-            animator.SetBool("isRunning", false);
+            animator.SetBool("isRunning", false); //set it back to false if no movement input
+
+        if (Input.GetAxis("Jump") != 0)
+        {
+            animator.SetBool("isJumping", true);
+        }
+        else 
+            animator.SetBool("isJumping", false);
 
     }
 }
