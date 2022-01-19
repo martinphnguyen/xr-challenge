b63a36411d9a71269c4c1b5f1eaf06bf589cd97a
Martin Nguyen <45037438+martinphnguyen@users.noreply.github.com>
2022-01-19 18:01:56 +0000

    Timer countdown for score

--- Assets/Scripts/Scores.cs
+++ Assets/Scripts/Scores.cs
@@ -15,30 +15,45 @@ public class Scores : MonoBehaviour
     string hScore = "highscore";
 
     float timer;
+    public Text timerText;
+    bool gameOver;
 
+    Scene currentScene;
     // Start is called before the first frame update
     void Start()
     {
-        Scene currentScene = SceneManager.GetActiveScene(); //seeing what scene we are in to do different things
+        currentScene = SceneManager.GetActiveScene(); //seeing what scene we are in to do different things
         if (currentScene.name == "Main") //game scene
         {
-        score = 0; //set as default
-        scoreText.text = "Score: " + score.ToString(); //displaying score
+            score = 0; //set as default
+            scoreText.text = "Score: " + score.ToString(); //displaying score
+            timer = 12.0f;
+            timerText.text = timer.ToString("F0"); //displaying timer
         }
-        else 
-            if( currentScene.name == "Scores")
+        else
+            if (currentScene.name == "Scores")
         {
             DisplayScore();
         }
 
-
+        gameOver = false;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (currentScene.name == "Main")
+        {
+            timer -= Time.deltaTime;
+            timerText.text = timer.ToString("F0"); //displaying timer
+        }
+
+        if (timer < 0.0f && !gameOver)
+        {
+            gameOver = true;
+            GameOver();
+        }
     }
 
     public void AddScore(int addScore)
@@ -55,6 +70,7 @@ public class Scores : MonoBehaviour
 
     public void SaveScore(int score)
     {
+        int newScore = score * (int)timer;
         PlayerPrefs.SetInt(lScore, score);
 
         int high = PlayerPrefs.GetInt(hScore);
@@ -69,5 +85,12 @@ public class Scores : MonoBehaviour
         scoreText.text = PlayerPrefs.GetInt(lScore).ToString();
 
         highscoreText.text = PlayerPrefs.GetInt(hScore).ToString();
+    }
+
+    public void GameOver()
+    {
+
+        SaveScore(0);
+        SceneManager.LoadScene("Scores");
     }
 }
